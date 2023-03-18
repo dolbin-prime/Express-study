4170b92ce7de15a10bbecbec4c6582122d1b2d81
Sukbin Lee <lunarlsb@gmail.com>
2023-03-19 01:27:32 +0900

    [Add] Middleware first step

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
 var express = require('express');
+
 var app = express();
 const port = 18722;
 
-app.get('/', function (req, res) {
-    let options = {
-        root: 'C:/Users/LSB/Documents/GitHub/ExpressJsStudy'
-    };
-    // res.send('Hello World!');
-    res.sendFile('/maybe-a-file', options);
-});
+const myLogger = function (req, res, next) {
+    console.log('LOGGED');
+    next();
+};
+
+const requestTime = function (req, res, next) {
+    req.requestTime = Date.now();
+    next();
+}
+
 
 app.listen(port, function () {
     console.log(`Example app listening on port ${port}!`);
 });
+
+app.use(myLogger);
+app.use(requestTime);
+
+app.get('/', (req, res) => {
+    let responseText = 'Hello World!<br>';
+    responseText += `<small>Requested at: ${req.requestTime}</small>`;
+    res.send(responseText);
+})
